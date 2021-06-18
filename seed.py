d141413640f6103d4b269faa7e42f86c3a0d26c5
Thomas Balunis <thomfb3@gmail.com>
2021-06-17 22:24:51 -0400

    Upgrade Python 3.9.5

--- seed.py
+++ seed.py
@@ -1,27 +1,20 @@
 from models import db, User, UserRecipes, MealTime, Permissions
 
-def run_seed():
-    db.drop_all()
-    db.create_all()
+db.drop_all()
+db.create_all()
 
 
-    p1 = Permissions(
-        permissions="basic"
-    )
+p1 = Permissions(
+    permissions="basic"
+)
 
 
-    db.session.add(p1)
-    db.session.commit()
+db.session.add(p1)
+db.session.commit()
 
 
-    u1 = User.signup("Thomfb3", "Tom", "password", "/static/images/default-pic.png")
-    
-    # u1 = User(
-    #     username="Thomfb3",
-    #     first_name="Tom",
-    #     password="password",
-    #     permissions=1
-    # )
+u1 = User.signup("Thomfb3", "Tom", "password", "/static/images/default-pic.png")
 
-    db.session.add(u1)
-    db.session.commit()
+
+db.session.add(u1)
+db.session.commit()
